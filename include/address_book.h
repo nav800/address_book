1e2c7b8decf6485d528b98c044048d8c7ea4b090
Abhinav Nair <53712831+nav800@users.noreply.github.com>
2024-12-17 14:39:27 -0800

    added class defs for address book and contact

--- include/address_book.h
+++ include/address_book.h
@@ -1,1 +1,16 @@
+#ifndef addressBook_H
+#define addressBook_H
 
+#include <string>
+
+class AddressBook{
+
+};
+
+class Contact{
+  std::string name;
+  std::string email;
+  int number;
+};
+
+#endif
