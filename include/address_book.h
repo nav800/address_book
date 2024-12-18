776747d3535e1db1efc91476b0e7f9968dc97b1f
Abhinav Nair <53712831+nav800@users.noreply.github.com>
2024-12-17 22:53:30 -0800

    Update address_book.h

--- include/address_book.h
+++ include/address_book.h
@@ -3,20 +3,19 @@
 
 #include <string>
 
-class AddressBook{
-  class Iterator{
-    AddressBook _book;
-    int _index;
-  }
-int size;
-Contact* _contacts;
-
-};
-
 class Contact{
   std::string _name;
   std::string _email;
   int _number;
+};
+
+class AddressBook{
+  class Iterator{
+    int _index;
+  };
+int size;
+Contact* _contacts;
+
 };
 
 #endif
