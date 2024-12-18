6da28470db011d7e8de11dce4acd25d412030bef
Abhinav Nair <53712831+nav800@users.noreply.github.com>
2024-12-17 16:29:04 -0800

    Added iterator and member fields
    Added iterator class definition
    Added private members for contact, book, and book iterator

--- include/address_book.h
+++ include/address_book.h
@@ -4,13 +4,19 @@
 #include <string>
 
 class AddressBook{
+  class Iterator{
+    AddressBook _book;
+    int _index;
+  }
+int size;
+Contact* _contacts;
 
 };
 
 class Contact{
-  std::string name;
-  std::string email;
-  int number;
+  std::string _name;
+  std::string _email;
+  int _number;
 };
 
 #endif
