400ba7fa4322926e35e6e6f4656b94679d248ccf
Abhinav Nair <53712831+nav800@users.noreply.github.com>
2024-12-18 20:06:26 -0800

    Update main.cpp
    Create sample contact and book in main
    Add contact to book in main

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,9 @@
 #include <iostream>
 
 int main() {
+  Contact c = Contact("Hi", 123, "there");
+  AddressBook b = AddressBook(4);
+  b.push_back(c);
   std::cout << "Enter contact: ";
   return 0;
 }
