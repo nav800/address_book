6a99a5f2df4c75e6f4ee3364989dbd3f39944efd
Abhinav Nair <53712831+nav800@users.noreply.github.com>
2024-12-19 00:23:55 -0800

    editing and adding contacts

--- include/address_book.h
+++ include/address_book.h
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <vector>
+#include <iostream>
 
 class Contact{
   std::string name;
@@ -15,6 +16,26 @@ class Contact{
   bool empty(){
     return (name == "Name empty" && number == -8 && email == "No email given");
   }
+  void edit(const std::string& _name = "Name empty", const size_t _number = -8, const std::string& _email = "No email given"){
+    if (_name != "Name empty"){
+      name = _name;
+    }
+    if (_number != -8){
+      number = _number;
+    }
+    if (_email != "No email given"){
+      email = _email;
+    }
+  }
+  std::string Name(){
+    return name;
+  }
+  int Number(){
+    return number;
+  }
+  std::string Email(){
+    return email;
+  }
 };
 
 class AddressBook{
@@ -22,5 +43,23 @@ class AddressBook{
   
   public:
     AddressBook() {}
+    void add_contact(std::string name, int number, std::string email){
+      Contact temp = Contact(name, number, email);
+      _contacts.push_back(temp);
+    }
+    void add_contact(Contact temp){
+      _contacts.push_back(temp);
+    }
+    void display(Contact c){
+      std::cout << "Name: " << c.Name() << ", Number: " << c.Number() << ", Email: " << c.Email() << std::endl;
+    }
+    void display_all(){
+      for (Contact c: _contacts){
+        display(c);
+      }
+    }
+    void edit_index(int index, std::string name, int number, std::string email){
+      _contacts[index].edit(name, number, email);
+    }
 };
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,30 @@
 #include "../include/address_book.h"
-#include <iostream>
 
 int main() {
+  std::string t_name, t_email;
+  int t_number=0;
+  int t_index;
+  std::cout << "Enter name: ";
+  std::cin >> t_name;
+  std::cout << "Enter number: ";
+  std::cin >> t_number;
+  std::cout << "Enter email: ";
+  std::cin >> t_email;
+  Contact d = Contact(t_name, t_number, t_email);
   Contact c = Contact("Hi", 123, "there");
   AddressBook b = AddressBook();
-  std::cout << "Enter contact: ";
+  b.add_contact(c);
+  b.add_contact(d);
+  b.display_all();
+  std::cout << "Enter index:";
+  std::cin >> t_index;
+  std::cout << "Enter name: ";
+  std::cin >> t_name;
+  std::cout << "Enter number: ";
+  std::cin >> t_number;
+  std::cout << "Enter email: ";
+  std::cin >> t_email;
+  b.edit_index(t_index, t_name, t_number, t_email);
+  b.display_all();
   return 0;
 }
